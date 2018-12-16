762e232ecc3ef8a76d6aa4b860fd92146a3f56df
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-16 22:08:41 +0900

    [Update] Create an infinite scrolling ListView

--- yoshi/first_flutter_app_pt1/lib/main.dart
+++ yoshi/first_flutter_app_pt1/lib/main.dart
@@ -1,24 +1,16 @@
+// Create an infinite scrolling lazily loaded list
+
 import 'package:flutter/material.dart';
 import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
   @override
-  Widget build(BuildContext context) { 
-    // final wordPair = new WordPair.random();
+  Widget build(BuildContext context) {
     return new MaterialApp(
-      title: 'Welcome to Flutter',
-      home: new Scaffold(
-        appBar: new AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: new Center(
-          //child: const Text('Hello World'),
-          //child: new Text(wordPair.asPascalCase),
-          child: new RandomWords(),
-        ),
-      ),
+      title: 'Startup Name Generator',
+      home: new RandomWords(),
     );
   }
 }
@@ -28,10 +20,41 @@ class RandomWords extends StatefulWidget {
   RandomWordsState createState() => new RandomWordsState();
 }
 
-class RandomWordsState extends State<RandomWords>{
+class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+
   @override
-  Widget build(BuildContext context){
-    final WordPair wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: const Text('Startup Name Generator'),
+      ),
+      body: _buildSuggestions(),
+    );
+  }
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: (BuildContext _context, int i) {
+          if (i.isOdd) {
+            return const Divider();
+          }
+          final int index = i ~/ 2;
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10));
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _biggerFont,
+      ),
+    );
   }
 }
