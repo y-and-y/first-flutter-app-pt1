cfbea5703859a8ce36643d1c2dc407caeb44ca88
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-12-16 20:40:44 +0900

    [Update] RandomWordsState

--- yoshi/first_flutter_app_pt1/lib/main.dart
+++ yoshi/first_flutter_app_pt1/lib/main.dart
@@ -1,20 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
 
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget{
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context) { 
+    // final wordPair = new WordPair.random();
     return new MaterialApp(
       title: 'Welcome to Flutter',
       home: new Scaffold(
         appBar: new AppBar(
           title: const Text('Welcome to Flutter'),
         ),
-        body: const Center(
-          child: const Text('Hello World'),
+        body: new Center(
+          //child: const Text('Hello World'),
+          //child: new Text(wordPair.asPascalCase),
+          child: new RandomWords(),
         ),
       ),
     );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords>{
+  @override
+  Widget build(BuildContext context){
+    final WordPair wordPair = new WordPair.random();
+    return new Text(wordPair.asPascalCase);
   }
 }
